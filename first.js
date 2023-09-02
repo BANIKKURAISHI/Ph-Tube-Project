fb8927d8119e641a81b2c5afdda1a9a8b78d4db1
BANIKKURAISHI <mdkuraishirahmanbanik@gmail.com>
2023-09-02 18:59:50 +0600

    this is third commit

--- first.js
+++ first.js
@@ -1,3 +1,5 @@
+
+//////////////option api////////////////////////////////////////////////////////////////////////////////////
 const  collection=async()=>{
   const other =await fetch(' https://openapi.programming-hero.com/api/videos/categories ')
   const data =await other.json()
@@ -5,55 +7,83 @@ const  collection=async()=>{
  
  console.log(data.data)  
  const contain =document.getElementById('contain')
+
+
  data.data.slice(0,4).forEach((category) => {
   const div=document.createElement('button')
   div.innerHTML=`
-  <button class="m-20 bg-neutral-300 p-4 rounded-md" onclick="searchBar('${category.category_id}')" >${category.category}</button>
+  <button id="modal" class="m-20 bg-neutral-300 p-4 rounded-md focus:bg-red-600" onclick="searchBar('${category.category_id}')" >${category.category}</button>
    `
   contain.appendChild(div)
  })
 
-
-};
- 
+}
+ /////////////////////category show api//////////////////////////////////////////////////////////////////////////
 const searchBar = async(categoryId)=>{
   
   const response  =await fetch(`https://openapi.programming-hero.com/api/videos/category/ ${categoryId} `)
   const data =await response.json()
-  console.log(data.data)
+
   const section=document.getElementById('card')
-  section.innerHTML=""
-  data.data?.forEach((profile) => {
-    
-    
-    const div=document.createElement('div')
-    div.innerHTML=`<div class="card w-96 bg-base-100 shadow-xl" >
+    section.innerHTML=""
+  
+   console.log(data.data)
    
-    <figure><img src="${profile?.thumbnail}" class="w-full h-52"></img></figure>
-  </div>
-  <div class="card-body text-justify">
-  <div class="flex >
-  <figure class="text-left -mx-4"><img src="${profile?.authors[0].profile_picture}" class="w-24 h-24 rounded-full">
-  </img>
-  <div class ="mx-5">
-  <h2 class="card-title">${profile?.title}</h2>
-  <h3>${profile?.authors[0].profile_name } </h3>
-  <span>${profile?.authors[0].verified   }</span>
-  <h4>${profile?.others.views } views</h4>
-  </div>
-  </figure>
+    data.data?.forEach((profile) => {
+      
+      
+      const div=document.createElement('div')
+   
   
-  </div>
-   </div>
   
-  `
+      div.innerHTML=`<div class="card w-80 bg-base-100 shadow-xl my-14 mx-8" >
+     
+      <figure><img src="${profile?.thumbnail}
+      " class="w-full h-52"></img>
+      
+      </figure>
+      <p class="text-right -my-10 text-white">${profile?.others.posted_date?Math.floor(profile?.others.posted_date*0.00277777778):'' }${profile?.others.posted_date?"hrs":''}
+      ${profile?.others.posted_date?Math.floor(profile?.others.posted_date*0.0166666667):'' }${profile?.others.posted_date?"min ago":''}
+      </p>
+    </div>
+    <div class="card-body text-justify">
+    <div class="flex  >
+    <figure class="text-left -mx-4"><img src="${profile?.authors[0].profile_picture}" class="w-24 h-24 rounded-full">
+    </img>
+    <div class ="mx-5 my-4">
+    <h2 class="card-title">${profile?.title}</h2>
+    <h3 class="picture">${profile?.authors[0].profile_name }
+    <span>
+     ${profile?.authors[0].verified ?'<i class="fa-solid fa-certificate">':'' }</i> </span>  </h3>
+           
+               
+    <h4>${profile?.others.views } views</h4>
+  
+   
+
+    </div>
+    </figure>
+    
+    </div>
+     </div>
+    
+    ` 
+    
+  
+      section.appendChild(div)})
+
+   
+      
+  }
+
+  //////convert 
+
   
 
-    section.appendChild(div)})
+  ////////////////////////////function call list
+  searchBar('1000')
+ 
+  collection()
+ 
 
-console.log(data.data)
-}
-
-
- collection()
- searchBar('1000')
+ 
--- second.js
+++ second.js
@@ -1,27 +1,23 @@
+const blogButton=()=>{
+ window.location.href="http://127.0.0.1:5500/question.html"
+}
 
-
-/* const  collection2=async()=>{
-  const other =await fetch(' https://openapi.programming-hero.com/api/videos/category/1000 ')
+const  collection2=async()=>{
+  const other =await fetch(`https://openapi.programming-hero.com/api/videos/category/ ${categoryId} `)
   const data =await other.json()
-  data.data.forEach((category) => {
-    const section=document.getElementById('card')
-    const div=document.createElement('div')
-    div.innerHTML=`<div class="card w-96 bg-base-100 shadow-xl">
-    <figure><${category
 
-    }></figure>
-    <div class="card-body">
-      <h2 class="card-title">Shoes!</h2>
-      <p>If a dog chews shoes whose shoes does he choose?</p>
-      </div>
-  </div>`
-  
-
-    section.appendChild(div)})
-    console.log(data.data)
-
- }*/
+ 
 
 
 
+}
 
+
+//""status":false,"message":"no data found!!!","data":[]}":false,"message":"no data found!!!","data":[]}
+
+// <h1> ${data.message}</h1> 
+
+/*<!-- The button to open modal -->
+*/
+
+//<span>${profile?.others.posted_date}</span>
